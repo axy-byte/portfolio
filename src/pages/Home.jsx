60de6259670d40012e4bd6b40b8b31d0515b30bf
mike4098-rts <U10604@rts-365.dk>
2023-04-04 23:14:15 +0200

    reactSETUP,TailwindCONFIGURED

---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div className="bg-primary">
+    <h1 className='text-primaryText font-primaryFont flex justify-center '>Portfolio Site</h1>
+  </div>
+  )
+}
